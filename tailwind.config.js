67112b018cdfcbb1ee4d06ff1d93404d17ad2c43
Talha naeem <75784944+talhanaeem347@users.noreply.github.com>
2022-12-02 22:47:21 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}
